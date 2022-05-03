bcdd2d8efa23a6168370e48b9f272d8ac916c3be
Dany_Lopez <irma.lopez@edu.uag.mx>
2022-05-03 11:37:05 -0500

    Ultimos cambios

--- common/models/user.js
+++ common/models/user.js
@@ -1,16 +1,21 @@
 'use strict';
 
 module.exports = function(Users) {
-    Users.login = async function (request, response){
-        req=request.json('');
-        return req;
+    Users.login = async function (email,password){
+        const Usr = Users.app.loopback.getModel("User");
+        return Usr.login({
+            email,
+            password
+        });
     }
     Users.remoteMethod(
         'login',{
             http: { verb: 'post'},
-            returns:[
-                {args: 'message', type:'string'},
-            ]
+            accepts: [
+                { arg: "email", type: "string" },
+                { arg: "password", type: "string" },
+            ],
+            returns: { arg: "response", type: "object", root: true }
         }
     )
 };
